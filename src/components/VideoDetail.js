6dd875cec5ff4e04e69e9c6edb06be5ba1b5261c
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2020-12-25 13:28:41 +0530

    indentation issue resolved

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -8,20 +8,16 @@ const VideoDetail = ({ video }) => {
 
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
-    return ( <
-        div >
-        <
-        div className = "ui embed" >
-        <
-        iframe src = { videoSrc }
-        /> <
-        /div> <
-        div className = "ui segment" >
-        <
-        h4 className = "ui header" > { video.snippet.title } < /h4> <
-        p > { video.snippet.description } < /p> <
-        /div> <
-        /div>
+    return (
+        <div >
+                <div className="ui embed" >
+                    <iframe src={videoSrc} />
+                </div>
+                <div className="ui segment">
+                    <h4 className="ui header"> {video.snippet.title} </h4>
+                    <p > {video.snippet.description} </p>
+                </div>
+            </div>
     );
 }
 
