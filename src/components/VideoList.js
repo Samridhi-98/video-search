37b1cdf9d84608c82bdb6dd65db9ba042c5e59f4
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-01-01 13:26:02 +0530

    minor bug fixed 2.0

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,15 +6,17 @@ const VideoList = ({ videos, onVideoSelect }) => {
 
     const renderedList = videos.map(video => {
         return <VideoItem
-        videoprops = { video }
-        onVideoSelect = { onVideoSelect }
-        key = { video.id.videoId }
-        /> ;
+            videoprops={video}
+            onVideoSelect={onVideoSelect}
+            key={video.id.videoId}
+        />;
     });
 
-    return ( <
-        div className = " ui relaxed divided list" > { /* We have {videos.length}  search results. */ } { renderedList } <
-        /div>
+    return (
+        <div className=" ui relaxed divided list" >
+            { /* We have {videos.length}  search results. */}
+            { renderedList}
+        </div>
     );
 }
 
