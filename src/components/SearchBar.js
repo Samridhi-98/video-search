f25772eafa2cb0b2644459cd2bdd4f7b0463c44a
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-01-19 13:24:04 +0530

    minor bug fixed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-class SearchBar extends React.Component{
-    
-    state={content:''};
+//class based component
+class SearchBar extends React.Component {
+
+    state = { content: '' };
 
     onFormSubmit = (event) => {
         event.preventDefault();
@@ -10,14 +11,14 @@ class SearchBar extends React.Component{
         this.props.onSearch(this.state.content);
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="ui segment">
                 <form className="ui form" onSubmit={this.onFormSubmit}>
-                   <div className="field">
-                       <label>Video Search</label>
-                       <input type="text" value={this.state.content}  onChange={event => this.setState({content: event.target.value})} />
-                   </div>
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input type="text" value={this.state.content} onChange={event => this.setState({ content: event.target.value })} />
+                    </div>
                 </form>
             </div>
         );
