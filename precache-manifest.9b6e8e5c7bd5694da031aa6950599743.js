29231a5ce399c60ab01b44872e7f38dfef3e91b4
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-04-06 20:52:19 +0530

    Updates

---
+++ precache-manifest.9b6e8e5c7bd5694da031aa6950599743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3365689199095f6ea04229ecbba4637",
+    "url": "/video-search/index.html"
+  },
+  {
+    "revision": "cd81bd2abdd5f7052f24",
+    "url": "/video-search/static/css/main.b0b36004.chunk.css"
+  },
+  {
+    "revision": "fb1b1413296b046c555d",
+    "url": "/video-search/static/js/2.f30102a1.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/video-search/static/js/2.f30102a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd81bd2abdd5f7052f24",
+    "url": "/video-search/static/js/main.257aa4e5.chunk.js"
+  },
+  {
+    "revision": "19482773e4ea79a7388c",
+    "url": "/video-search/static/js/runtime-main.5a4e9f6e.js"
+  }
+]);
